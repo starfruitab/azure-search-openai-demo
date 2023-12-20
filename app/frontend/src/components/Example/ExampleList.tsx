d4107aae7c1fa5d12f1b4c4a7cd8bb0329e5012f
Niklas Paasonen <n.paasonen@yahoo.com>
2023-12-20 13:33:43 +0100

    Update example

--- app/frontend/src/components/Example/ExampleList.tsx
+++ app/frontend/src/components/Example/ExampleList.tsx
@@ -11,7 +11,7 @@ const EXAMPLES: ExampleModel[] = [
     { text: "Explain alarm 11030461/11030462", value: "Explain alarm 110303461/110303462" },
     // { text: "How do I check photocell of the CSU frame?", value: "How do I check photocell of the CSU frame?" },
     { text: "How do I lubricate the linear unit?", value: "How do I lubricate the linear unit?" },
-    { text: "Describe the setting sequence of CAU", value: "Describe the setting sequence of CAU" },
+    { text: "Remove the bearing unit (Geared motor)", value: "How to remove the bearing unit (Geared motor)" },
     { text: "Check belt of the Servo lift discharger", value: "Check belt of the Servo Lift Discharger" }
 ];
 
